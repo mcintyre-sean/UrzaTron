f4f4eea3c91fe777f6c2ea9658484f4129f415cd
Sean McIntyre <seanrob77@gmail.com>
2017-06-08 16:31:14 -0700

    Commit with some comments

---
+++ bot.js
@@ -0,0 +1,38 @@
+var token = '306932229:AAFcETOj_bEIuzMmzL-DneQ7a_hceXNd2CU';
+var express = require('express');
+var packageInfo = require('./package.json');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Web server started at http://%s:%s', host, port);
+});
+
+var Bot = require('node-telegram-bot-api'),
+    bot = new Bot(token, { polling: true });
+
+console.log('bot server started...');
+
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, 'Greetings, ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
+
+bot.onText(/^\/sum((\s+\d+)+)$/, function (msg, match) {
+  var result = 0;
+  match[1].trim().split(/\s+/).forEach(function (i) {
+    result += (+i || 0);
+  })
+  bot.sendMessage(msg.chat.id, result).then(function () {
+    // reply sent!
+  });
+});
